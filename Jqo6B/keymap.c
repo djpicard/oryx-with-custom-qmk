c09bd4ad1b4ce527da1d6835f3d510a4c22ba848
DJ Picard <picard.daniel.cet@gmail.com>
2025-06-03 09:22:18 -0400

    Making cleaner arcane implementation

--- Jqo6B/keymap.c
+++ Jqo6B/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_MAC_UNDO,    KC_MAC_CUT,     KC_MAC_COPY,    KC_MAC_PASTE,   KC_NO,                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_NO,          KC_HOME,        KC_UP,          KC_END,         KC_PAGE_UP,                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_NO,          KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_NO,                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_PC_UNDO,     KC_PC_CUT,      KC_PC_COPY,     KC_PAUSE,       KC_FIND,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_PC_UNDO,     KC_PC_CUT,      KC_PC_COPY,     KC_PC_PASTE,       KC_FIND,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [3] = LAYOUT_voyager(
@@ -132,11 +132,6 @@ bool rgb_matrix_indicators_user(void) {
 bool remember_last_key_user(uint16_t keycode, keyrecord_t* record,
                             uint8_t* remembered_mods) {
       switch (keycode) {
-        case CW_TOGG:
-        case KC_ESC:
-        case KC_BSPC:
-        case KC_DEL:
-
         case LMAGIC:
         case RMAGIC:
            return false;  // Magic keys will ignore the above keycodes.
@@ -147,106 +142,60 @@ bool remember_last_key_user(uint16_t keycode, keyrecord_t* record,
 // An enhanced version of SEND_STRING: if Caps Word is active, the Shift key is
 // held while sending the string. Additionally, the last key is set such that if
 // the Repeat Key is pressed next, it produces `repeat_keycode`.
-#define MAGIC_STRING(str, repeat_keycode) \
-        magic_send_string_P(PSTR(str), (repeat_keycode))
- 
-static void magic_send_string_P(const char* str, uint16_t repeat_keycode) {
-    uint8_t saved_mods = 0;
- 
-  if (is_caps_word_on()) { // If Caps Word is on, save the mods and hold Shift.
-    saved_mods = get_mods();
-    register_mods(MOD_BIT(KC_LSFT));
-  }
- 
-  send_string_with_delay_P(str, TAP_CODE_DELAY);  // Send the string.
-  set_last_keycode(repeat_keycode); // 2024-03-09 Disabled sending of string for mag-rep / rep-mag consistency.
- 
-  // If Caps Word is on, restore the mods.
-  if (is_caps_word_on()) {
-    set_mods(saved_mods);
-  }
-}
-
 static void process_left_magic(uint16_t keycode, uint8_t mods) { // LMAGIC definitions
     switch (keycode) {
-        case  KC_A: { MAGIC_STRING("a",         KC_SPC); } break;
-        case  KC_B: { MAGIC_STRING("ecause",    KC_NO); } break;
-        case  KC_C: { MAGIC_STRING("an",        KC_NO); } break;
-      //case  KC_D: { MAGIC_STRING("d",         KC_NO); } break;
-        case  KC_E: { MAGIC_STRING("e",         KC_NO); } break;
-      //case  KC_F: { MAGIC_STRING("f",         KC_NO); } break;
-        case  KC_G: { MAGIC_STRING("eneral",    KC_NO); } break;
-        case  KC_H: { MAGIC_STRING("h",         KC_NO); } break;
-        case  KC_I: { MAGIC_STRING("i",         KC_NO); } break;
-        case  KC_J: { MAGIC_STRING("ust",       KC_NO); } break;
-      //case  KC_K: { MAGIC_STRING("k",         KC_NO); } break;
-      //case  KC_L: { MAGIC_STRING("l",         KC_NO); } break;
-        case  KC_M: { MAGIC_STRING("ent",       KC_NO); } break;
-        case  KC_N: { MAGIC_STRING("ion",       KC_NO); } break;
-        case  KC_O: { MAGIC_STRING("o",         KC_NO); } break;
-        case  KC_P: { MAGIC_STRING("retty",     KC_NO); } break;
-        case  KC_Q: { MAGIC_STRING("q",         KC_NO); } break;
-      //case  KC_R: { MAGIC_STRING("r",         KC_NO); } break;
-      //case  KC_S: { MAGIC_STRING("s",         KC_NO); } break;
-        case  KC_T: { MAGIC_STRING("t",         KC_NO); } break;
-        case  KC_U: { MAGIC_STRING("u",         KC_NO); } break;
-        case  KC_V: { MAGIC_STRING("azjorfia",  KC_NO); } break;
-        case  KC_W: { MAGIC_STRING("hich",      KC_NO); } break;
-        case  KC_X: { MAGIC_STRING("x",         KC_NO); } break;
-        case  KC_Y: { MAGIC_STRING("y",         KC_NO); } break;
-        case  KC_Z: { MAGIC_STRING("z",         KC_NO); } break;
- 
-        case KC_COMM: { MAGIC_STRING(" and",    KC_NO); } break;
-    }
+      // right hand
+        case  KC_B: { SEND_STRING("ecause"); } break;
+        case  KC_C: { SEND_STRING("an"); } break;
+        case  KC_D: { SEND_STRING("d"); } break;
+        case  KC_F: { SEND_STRING("f"); } break;
+        case  KC_G: { SEND_STRING("eneral"); } break;
+        case  KC_J: { SEND_STRING("ust"); } break;
+        case  KC_K: { SEND_STRING("k"); } break;
+        case  KC_L: { SEND_STRING("l"); } break;
+        case  KC_M: { SEND_STRING("ent"); } break;
+        case  KC_N: { SEND_STRING("ion"); } break;
+        case  KC_P: { SEND_STRING("retty"); } break;
+        case  KC_R: { SEND_STRING("n"); } break;
+        case  KC_S: { SEND_STRING("s"); } break;
+        case  KC_T: { SEND_STRING("t"); } break;
+        case  KC_V: { SEND_STRING("azjorfia"); } break;
+        case  KC_W: { SEND_STRING("hich"); } break;
+        case  KC_Z: { SEND_STRING("z"); } break;
+        default: SEND_STRING(keycode); break;
+     }
 }
  
 static void process_right_magic(uint16_t keycode, uint8_t mods) { // RMAGIC definitions
     switch (keycode) {
-        case  KC_A: { MAGIC_STRING("nd",        KC_SPC); } break;
-        case  KC_B: { MAGIC_STRING("b",         KC_NO); } break;
-        case  KC_C: { MAGIC_STRING("c",         KC_NO); } break;
-        case  KC_D: { MAGIC_STRING("d",         KC_NO); } break;
-      //case  KC_E: { MAGIC_STRING("e",         KC_NO); } break;
-        case  KC_F: { MAGIC_STRING("f",         KC_NO); } break;
-        case  KC_G: { MAGIC_STRING("g",         KC_NO); } break;
-      //case  KC_H: { MAGIC_STRING("h",         KC_NO); } break;
-        case  KC_I: { MAGIC_STRING("on",        KC_NO); } break;
-        case  KC_J: { MAGIC_STRING("j",         KC_NO); } break;
-        case  KC_K: { MAGIC_STRING("k",         KC_NO); } break;
-        case  KC_L: { MAGIC_STRING("l",         KC_NO); } break;
-        case  KC_M: { MAGIC_STRING("m",         KC_NO); } break;
-        case  KC_N: { MAGIC_STRING("n",         KC_NO); } break;
-      //case  KC_O: { MAGIC_STRING("o",         KC_NO); } break;
-        case  KC_P: { MAGIC_STRING("p",         KC_NO); } break;
-        case  KC_Q: { MAGIC_STRING("ueen",      KC_NO); } break;
-        case  KC_R: { MAGIC_STRING("r",         KC_NO); } break;
-        case  KC_S: { MAGIC_STRING("s",         KC_NO); } break;
-      //case  KC_T: { MAGIC_STRING("t",         KC_NO); } break;
-      //case  KC_U: { MAGIC_STRING("u",         KC_NO); } break;
-        case  KC_V: { MAGIC_STRING("v",         KC_NO); } break;
-        case  KC_W: { MAGIC_STRING("w",         KC_NO); } break;
-        case  KC_X: { MAGIC_STRING("ex",        KC_NO); } break;
-        case  KC_Y: { MAGIC_STRING("eah",       KC_NO); } break;
-      //case  KC_Z: { MAGIC_STRING("z",         KC_NO); } break;
- 
-        case KC_COMM: { MAGIC_STRING(" but",    KC_NO); } break;
-        case KC_SPC:  { MAGIC_STRING("the",     KC_NO); } break;
-        case KC_ASTR: { MAGIC_STRING("*N**or**dr**as**s**i**l**", KC_NO); } break;
+      // left hand
+        case  KC_Q: { SEND_STRING("u"); } break;
+        case  KC_Y: { SEND_STRING("ou"); } break;
+        case  KC_O: { SEND_STRING("o"); } break;
+        case  KC_U: { SEND_STRING("u"); } break;
+        case  KC_H: { SEND_STRING("h"); } break;
+        case  KC_I: { SEND_STRING("i"); } break;
+        case  KC_E: { SEND_STRING("e"); } break;
+        case  KC_A: { SEND_STRING("u"); } break;
+        case  KC_X: { SEND_STRING("x"); } break;
+        default: SEND_STRING(keycode); break;
+
     }
 }
 //---------------------------------------------------------------------------------
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
-  if (record->event.pressed) {
   switch (keycode) {
-        case LMAGIC: { process_left_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
-        case RMAGIC: { process_right_magic(get_last_keycode(), get_last_mods()); set_last_keycode(KC_SPC); } return false;
-        }
-    }
-
-  switch (keycode) {
-
+    case LMAGIC:
+      if (record->event.pressed) {
+        process_left_magic(get_last_keycode(), get_last_mods());
+      }
+      return false;
+    case RMAGIC:
+      if (record->event.pressed) {
+        process_right_magic(get_last_keycode(), get_last_mods());
+      }
+      return false;
     case DUAL_FUNC_0:
       if (record->tap.count > 0) {
         if (record->event.pressed) {
